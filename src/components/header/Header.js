719e5c3119c9e2c336bb71eca727dc1de23a912a
Carlos Rebelo <CarlosRebelo@github.com>
2018-07-18 11:53:28 +0100

    add Header

---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+import './Headline.css'
+
+let Header = () => {
+    return(
+        <div className='header'>
+            <h1 className='headline'>Awesome Store</h1>
+        </div>
+    )
+}
+
+export default Header
